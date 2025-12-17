bfeb8ebbdccb3dd25ca53ef32271db06a546a2da
nihuini <nihuini@tencent.com>
2025-12-17 19:24:18 +0800

    implement qwen3-vl-2b sample

---
+++ examples/qwen3_vl_main.cpp
@@ -0,0 +1,118 @@
+// Copyright 2025 Tencent
+// SPDX-License-Identifier: Apache-2.0
+
+#include "qwen3_vl_2b.h"
+#include "utils/prompt.h"
+#include <cstdio>
+#include <iostream>
+#include <string>
+#include <vector>
+
+#include <opencv2/core/core.hpp>
+#include <opencv2/highgui/highgui.hpp>
+
+#include "mat.h"
+#include "net.h"
+
+int main(int argc, char** argv)
+{
+    if (argc != 2)
+    {
+        fprintf(stderr, "usage: %s [image-path]\n", argv[0]);
+        return -1;
+    }
+
+    const char* image_path = argv[1];
+
+    cv::Mat bgr = cv::imread(image_path, 1);
+    if (bgr.empty())
+    {
+        fprintf(stderr, "cv::imread %s failed\n", image_path);
+        return -1;
+    }
+
+    qwen3_vl_2b model("./assets/qwen3_vl_2b/qwen3_vl_vision_embed_patch.ncnn.param",
+                      "./assets/qwen3_vl_2b/qwen3_vl_vision_embed_patch.ncnn.bin",
+                      "./assets/qwen3_vl_2b/qwen3_vl_vision_embed_pos.ncnn.param",
+                      "./assets/qwen3_vl_2b/qwen3_vl_vision_embed_pos.ncnn.bin",
+                      "./assets/qwen3_vl_2b/qwen3_vl_vision_encoder.ncnn.param",
+                      "./assets/qwen3_vl_2b/qwen3_vl_vision_encoder.ncnn.bin",
+                      "./assets/qwen3_vl_2b/qwen3_vl_embed_token.ncnn.param",
+                      "./assets/qwen3_vl_2b/qwen3_vl_embed_token.ncnn.bin",
+                      "./assets/qwen3_vl_2b/qwen3_vl_proj_out.ncnn.param",
+                      "./assets/qwen3_vl_2b/qwen3_vl_decoder.ncnn.param",
+                      "./assets/qwen3_vl_2b/qwen3_vl_decoder.ncnn.bin",
+                      "./assets/qwen3_vl_2b/vocab.txt",
+                      "./assets/qwen3_vl_2b/merges.txt",
+                       /*use_vulkan=*/false);
+
+    std::cout << "Chat with Qwen3-VL-2B! Type 'exit' or 'quit' to end the conversation.\n";
+
+    std::string prompt = apply_chat_template({
+        {"system", "You are a helpful assistant."},
+    }, {}, false, false);
+
+    // std::cout << "prompt = " << prompt << std::endl;
+
+    auto ctx = model.prefill(prompt);
+
+    {
+        // std::string user_input = "识别图片中的文字";
+        std::string user_input = "分析图片内容";
+        // std::string user_input = "Describe this image.";
+
+        std::string user_message = apply_chat_template({
+            {"user", "<|vision_start|><|image_pad|><|vision_end|>" + user_input}
+        }, {}, true);
+
+        // std::cout << "user_message = " << user_message << std::endl;
+
+        ctx = model.prefill(user_message, bgr, ctx);
+
+        GenerateConfig cfg;
+        cfg.beam_size = 1;
+        cfg.top_k = 20;
+        cfg.top_p = 0.8;
+        cfg.temperature = 0.7;
+        cfg.repetition_penalty = 1.0;
+        cfg.do_sample = true;
+        cfg.max_new_tokens = 8192;
+
+        ctx = model.generate(ctx, cfg, [](const std::string& token){
+            std::cout << token << std::flush;
+        });
+        std::cout << std::endl;
+    }
+
+    while (true) {
+        std::string input;
+        std::cout << "User: ";
+        std::getline(std::cin, input);
+        if (input == "exit" || input == "quit") {
+            break;
+        }
+        std::string user_message = apply_chat_template({
+            {"user", input}
+        }, {}, true);
+
+        ctx = model.prefill(user_message, ctx);
+
+        std::cout << "Assistant: ";
+
+        GenerateConfig cfg;
+        cfg.beam_size = 1;
+        cfg.top_k = 20;
+        cfg.top_p = 0.8;
+        cfg.temperature = 0.7;
+        cfg.repetition_penalty = 1.0;
+        cfg.do_sample = true;
+        cfg.max_new_tokens = 8192;
+
+        ctx = model.generate(ctx, cfg, [](const std::string& token){
+            std::cout << token << std::flush;
+        });
+        std::cout << std::endl;
+    }
+
+    return 0;
+}
---
+++ src/qwen3_vl_2b.h
@@ -0,0 +1,67 @@
+// Copyright 2025 Tencent
+// SPDX-License-Identifier: Apache-2.0
+
+#pragma once
+
+#include <array>
+#include <cassert>
+#include <functional>
+#include <locale>
+#include <memory>
+#include <string>
+#include <type_traits>
+#include <vector>
+
+#include <opencv2/core/core.hpp>
+
+#include "mat.h"
+
+struct GenerateConfig {
+    int max_new_tokens = 8192;
+    float temperature = 1.f;
+    float top_p = 1.f;
+    int top_k = 50;
+    float repetition_penalty = 1.05f;
+    int beam_size = 1;
+    int do_sample = 0;
+};
+
+struct qwen3_vl_2b_ctx;
+
+class qwen3_vl_2b {
+public:
+    qwen3_vl_2b(
+              std::string vision_embed_patch_param,
+              std::string vision_embed_patch_bin,
+              std::string vision_embed_pos_param,
+              std::string vision_embed_pos_bin,
+              std::string vision_encoder_param,
+              std::string vision_encoder_bin,
+              std::string embed_param,
+              std::string embed_bin,
+              std::string proj_out_param,
+              std::string decoder_param,
+              std::string decoder_bin,
+              std::string vocab_file,
+              std::string merges_file,
+              bool use_vulkan);
+
+    ~qwen3_vl_2b();
+
+    std::shared_ptr<qwen3_vl_2b_ctx> prefill(const std::string& input_text) const;
+
+    std::shared_ptr<qwen3_vl_2b_ctx> prefill(const std::string& input_text, const cv::Mat& bgr, const std::shared_ptr<qwen3_vl_2b_ctx> ctx) const;
+
+    std::shared_ptr<qwen3_vl_2b_ctx> prefill(const std::string& input_text, const std::shared_ptr<qwen3_vl_2b_ctx> ctx) const;
+
+    std::shared_ptr<qwen3_vl_2b_ctx> generate(const std::shared_ptr<qwen3_vl_2b_ctx>& ctx, const GenerateConfig& cfg, std::function<void(const std::string&)> callback) const;
+
+    bool decode(std::shared_ptr<qwen3_vl_2b_ctx> ctx, std::function<void(const std::string&)> callback) const;
+
+    // for vision encoder
+    int get_visiual_features(const cv::Mat& bgr, ncnn::Mat& image_embeds, std::vector<ncnn::Mat>& deepstack_features, int& num_patches_w, int& num_patches_h) const;
+
+private:
+    class Impl;
+    std::unique_ptr<Impl> impl_;
+};
